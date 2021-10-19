fa20590584cea80d34db112f7fc7f9ffc32a07be
18-Felix <91874612+18-Felix@users.noreply.github.com>
2021-10-19 15:42:25 +0800

    UPDATE
    GAME WORKS NOW

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    public float gravitySpeed;
     private Animation thisAnimation;
 
     void Start()
@@ -15,6 +17,23 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 1, 0) * gravitySpeed, ForceMode.Impulse);
+        }
+            
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Avoid")
+        {
+            GameManager.thisManager.UpdateScore(1);
+        }
+
+        if (collision.gameObject.tag == "Death")
+        {
+            SceneManager.LoadScene("Lose");
+        }
     }
 }
